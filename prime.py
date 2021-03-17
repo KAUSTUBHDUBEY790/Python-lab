a446ae4861391cf97f6386542c8141f42656e1c5
KAUSTUBHDUBEY790 <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2021-03-17 14:37:09 +0530

    Program to find prime or not

---
+++ prime.py
@@ -0,0 +1,8 @@
+a=eval(raw_input("Enter the number "))
+for i in range(2,a):
+          if a%i==0:
+                    print("Not Prime")
+                    break
+
+else:
+          print("Prime ")
